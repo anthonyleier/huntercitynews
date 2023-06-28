5bbda4e15b76fac5591f4721054161e88abb067a
Anthony Cruz <40519347+anthonyleier@users.noreply.github.com>
2023-06-28 19:00:37 +0000

    adicionado os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Hello World</h1>;
+  return <h1>Hello World</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("opa");
 }
 
 export default Home;
