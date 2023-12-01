c25b6629f46510f936889fef04af42e4831f8725
Anthony Cruz <anthonyleierlw@gmail.com>
2023-12-01 09:23:24 -0300

    Funções e testes para estatísticas do banco

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,7 +2,24 @@ import database from "infra/database.js";
 
 async function Status(request, response) {
   const updatedAt = new Date().toISOString();
-  response.status(200).json({ updated_at: updatedAt });
+
+  let result = await database.query("SHOW server_version;");
+  const postgresVersion = result.rows[0].server_version;
+
+  result = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(result.rows[0].max_connections);
+
+  result = await database.query(
+    "SELECT COUNT(*) AS opened_connections FROM pg_stat_activity;"
+  );
+  const openedConnections = parseInt(result.rows[0].opened_connections);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: postgresVersion,
+    max_connections: maxConnections,
+    opened_connections: openedConnections,
+  });
 }
 
 export default Status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,7 +4,14 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const responseBody = await response.json();
   expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.version).toBeDefined();
+  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.opened_connections).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.version).toBe("16.0");
+  expect(responseBody.max_connections).toBe(100);
+  expect(responseBody.opened_connections).toBe(6);
 });
