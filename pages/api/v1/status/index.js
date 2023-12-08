eb991d1870657dc259908c8fa1cc75bf259cc8df
Anthony Cruz <anthonyleierlw@gmail.com>
2023-12-08 09:27:26 -0300

    Melhoria na semântica das variáveis

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,16 +3,18 @@ import database from "infra/database.js";
 async function Status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  let result = await database.query("SHOW server_version;");
-  const postgresVersion = result.rows[0].server_version;
+  const postgresVersionResult = await database.query("SHOW server_version;");
+  const postgresVersion = postgresVersionResult.rows[0].server_version;
 
-  result = await database.query("SHOW max_connections;");
-  const maxConnections = parseInt(result.rows[0].max_connections);
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
 
-  result = await database.query(
+  const openedConnectionsResult = await database.query(
     "SELECT COUNT(*) AS opened_connections FROM pg_stat_activity;"
   );
-  const openedConnections = parseInt(result.rows[0].opened_connections);
+  const openedConnections = parseInt(
+    openedConnectionsResult.rows[0].opened_connections
+  );
 
   response.status(200).json({
     updated_at: updatedAt,
