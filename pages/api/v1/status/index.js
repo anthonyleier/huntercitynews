768bdba098e85c70553aa8ce0d990e965398ff21
Anthony Cruz <anthonyleierlw@gmail.com>
2023-12-20 08:24:08 -0300

    Melhoria no nome do banco

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -9,9 +9,11 @@ async function Status(request, response) {
   const maxConnectionsResult = await database.query("SHOW max_connections;");
   const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
 
-  const openedConnectionsResult = await database.query(
-    "SELECT COUNT(*)::INTEGER AS opened_connections FROM pg_stat_activity WHERE datname = 'local_db';"
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsResult = await database.query({
+    text: `SELECT COUNT(*)::INTEGER AS opened_connections FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
   const openedConnections = openedConnectionsResult.rows[0].opened_connections;
 
   response.status(200).json({
