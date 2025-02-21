03a98f23edccbe80e06b8f72d3476b2bdf6f4b20
Anthony Cruz <anthonyleierlw@gmail.com>
2025-02-21 17:15:59 -0300

    feat: refatora o controller de status com `next-connect`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,43 +1,52 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors";
-async function status(request, response) {
-  try {
-    const updatedAt = new Date().toISOString();
+import { InternalServerError, MethodNotAllowedError } from "infra/errors";
 
-    const postgresVersionResult = await database.query("SHOW server_version;");
-    const postgresVersion = postgresVersionResult.rows[0].server_version;
+const router = createRouter();
+router.get(getHandler);
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+});
 
-    const maxConnectionsResult = await database.query("SHOW max_connections;");
-    const maxConnections = parseInt(
-      maxConnectionsResult.rows[0].max_connections,
-    );
-
-    const databaseName = process.env.POSTGRES_DB;
-    const openedConnectionsResult = await database.query({
-      text: `SELECT COUNT(*)::INTEGER AS opened_connections FROM pg_stat_activity WHERE datname = $1;`,
-      values: [databaseName],
-    });
-    const openedConnections =
-      openedConnectionsResult.rows[0].opened_connections;
-
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: postgresVersion,
-          max_connections: maxConnections,
-          opened_connections: openedConnections,
-        },
-      },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({ cause: error });
-
-    console.log("\n Erro dentro do catch do controller:");
-    console.error(publicErrorObject);
-
-    response.status(500).json(publicErrorObject);
-  }
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
 
-export default status;
+function onErrorHandler(error, request, response) {
+  const publicErrorObject = new InternalServerError({ cause: error });
+
+  console.log("\n Erro dentro do catch do next-connect:");
+  console.error(publicErrorObject);
+
+  response.status(500).json(publicErrorObject);
+}
+
+async function getHandler(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const postgresVersionResult = await database.query("SHOW server_version;");
+  const postgresVersion = postgresVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsResult = await database.query({
+    text: `SELECT COUNT(*)::INTEGER AS opened_connections FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const openedConnections = openedConnectionsResult.rows[0].opened_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+      },
+    },
+  });
+}
