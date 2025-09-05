aba1d313540164be9ab8e8d3c37e7c0289460e52
Anthony Cruz <anthonyleierlw@gmail.com>
2025-09-05 17:01:52 -0300

    feat: adicionar suporte para verificação e manipulação de emails no orquestrador

--- tests/integration/infra/email.test.js
+++ tests/integration/infra/email.test.js
@@ -1,12 +1,32 @@
 import email from "infra/email.js";
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
 
 describe("infra/email.js", () => {
   test("send()", async () => {
+    await orchestrator.deleteAllEmails();
+
     await email.send({
       from: "HunterCityNews <contato@huntercitynews.com.br>",
       to: "Anthony <anthonyleierlw@gmail.com>",
       subject: "Teste de assunto",
       text: "Teste de corpo.",
     });
+
+    await email.send({
+      from: "HunterCityNews <contato@huntercitynews.com.br>",
+      to: "Anthony <anthonyleierlw@gmail.com>",
+      subject: "Último email enviado",
+      text: "Corpo do último email.",
+    });
+
+    const lastEmail = await orchestrator.getLastEmail();
+    expect(lastEmail.sender).toBe("<contato@huntercitynews.com.br>");
+    expect(lastEmail.recipients[0]).toBe("<anthonyleierlw@gmail.com>");
+    expect(lastEmail.subject).toBe("Último email enviado");
+    expect(lastEmail.text).toBe("Corpo do último email.\r\n");
   });
 });
