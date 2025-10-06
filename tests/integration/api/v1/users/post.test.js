94c6883bc85a7482da916e0c06ae6729615b8ef4
Anthony Cruz <anthonyleierlw@gmail.com>
2025-10-05 23:28:55 -0300

    feat: add default feature `read:activation_token` when creating `user`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -28,7 +28,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "anthonycruz",
         email: "anthonyleierlw@curso.dev",
-        features: [],
+        features: ["read:activation_token"],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
