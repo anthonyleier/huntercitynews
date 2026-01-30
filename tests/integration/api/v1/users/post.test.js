ca5178aac8daeac1cc6fca5e39cb501c4cde9e0a
Anthony Cruz <anthonyleierlw@gmail.com>
2026-01-30 13:42:15 -0300

    feat: adiciona filtro de output para método POST `/api/v1/users`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -27,9 +27,7 @@ describe("POST /api/v1/users", () => {
       expect(responseBody).toEqual({
         id: responseBody.id,
         username: "anthonycruz",
-        email: "anthonyleierlw@curso.dev",
         features: ["read:activation_token"],
-        password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
