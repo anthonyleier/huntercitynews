6d8a7d4a14bc568e0a92b851d2069f90dee4cf8c
Anthony Cruz <anthonyleierlw@gmail.com>
2026-08-13 15:49:39 -0300

    feat(cadastro): aplica componentes primer no lugar de html puro

--- pages/cadastro/index.js
+++ pages/cadastro/index.js
@@ -1,9 +1,25 @@
 import { useState } from "react";
+import { Button, FormControl, TextInput, Stack, Heading } from "@primer/react";
 import DefaultLayout from "interface/DefaultLayout";
 
 export default function RegisterPage() {
-  console.log("Render do <RegisterPage>");
+  return (
+    <DefaultLayout
+      contentWidth="small"
+      metadata={{
+        title: "Cadastro",
+        description: "Crie sua conta de forma gratuita.",
+      }}
+    >
+      <Stack gap="spacious">
+        <Heading as="h1">Cadastro</Heading>
+        <RegisterForm />
+      </Stack>
+    </DefaultLayout>
+  );
+}
 
+function RegisterForm() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -27,47 +43,50 @@ export default function RegisterPage() {
   }
 
   return (
-    <DefaultLayout
-      metadata={{
-        title: "Cadastro",
-        description: "Crie sua conta de forma gratuita.",
-      }}
-    >
-      <h1>Cadastro</h1>
-
-      <form onSubmit={handleSubmit}>
-        <div>
-          Nome de usuário:{" "}
-          <input
+    <form onSubmit={handleSubmit}>
+      <Stack gap="normal">
+        <FormControl>
+          <FormControl.Label>Nome de usuário</FormControl.Label>
+          <TextInput
             type="text"
             value={username}
             onChange={(event) => {
               setUsername(event.target.value);
             }}
+            block
           />
-        </div>
-        <div>
-          Email:{" "}
-          <input
+        </FormControl>
+
+        <FormControl>
+          <FormControl.Label>Email</FormControl.Label>
+          <TextInput
             type="text"
             value={email}
             onChange={(event) => {
               setEmail(event.target.value);
             }}
+            block
           />
-        </div>
-        <div>
-          Senha:{" "}
-          <input
+        </FormControl>
+
+        <FormControl>
+          <FormControl.Label>Senha</FormControl.Label>
+          <TextInput
             type="password"
             value={password}
             onChange={(event) => {
               setPassword(event.target.value);
             }}
+            block
           />
-        </div>
-        <button type="submit">Criar cadastro</button>
-      </form>
-    </DefaultLayout>
+        </FormControl>
+
+        <Stack.Item>
+          <Button type="submit" variant="primary">
+            Criar cadastro
+          </Button>
+        </Stack.Item>
+      </Stack>
+    </form>
   );
 }
