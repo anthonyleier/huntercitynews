fcfc399e3c5370130dd8f00b72c2843fa90819f1
Anthony Cruz <anthonyleierlw@gmail.com>
2026-09-04 14:12:30 -0300

    feat: faz PATCH na API para ativar o token/usuário

--- pages/cadastro/ativar/[activationTokenId].js
+++ pages/cadastro/ativar/[activationTokenId].js
@@ -1,10 +1,43 @@
 import DefaultLayout from "interface/DefaultLayout";
 import { Banner } from "@primer/react";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function ActivateUserPage() {
   const router = useRouter();
-  console.log(router.query.activationTokenId);
+
+  const activationTokenId = router.query.activationTokenId;
+
+  useEffect(() => {
+    if (!activationTokenId) return;
+
+    sendActivationRequest();
+
+    async function sendActivationRequest() {
+      try {
+        const response = await fetch(
+          `/api/v1/activations/${activationTokenId}`,
+          {
+            method: "PATCH",
+          },
+        );
+
+        const activationResponseBody = await response.json();
+
+        if (response.status === 200) {
+          console.log("Sucesso:", activationResponseBody);
+          // sucesso na interface
+          return;
+        }
+
+        // sinal de fracasso na interface
+        console.log("Fracasso:", activationResponseBody);
+      } catch {
+        // sinal de fracasso absoluto na interface
+        console.log("Fracasso2");
+      }
+    }
+  }, [activationTokenId]);
 
   return (
     <DefaultLayout contentWidth="small" metadata={{ title: "Ativar cadastro" }}>
